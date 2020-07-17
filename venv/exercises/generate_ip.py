c6f275c64d693e7b5a62fcbe4d7652a115e33feb
alexey.sergeenko <alexey.sergeenko@idfinance.com>
2020-07-17 15:02:12 +0300

    update file name

--- venv/exercises/generate_ip.py
+++ venv/exercises/generate_ip.py
@@ -1,33 +1,34 @@
 def generate_ipv4_list():
-		first = 0
-		result = []
-		while first < 1:
-				first += 1
-				second = 0
-				while second < 78:
-						second += 1
-						third = 0
-						fourth = 0
-						while third < 255:
-								third += 1
-								fourth = 1
-								while fourth < 255:
-										result.append(f'{first}.{second}.{third}.{fourth}')
-										fourth += 1
-						result.append(f'{first}.{second}.{third}.{fourth}')
-		return result
+	first_el = 0
+	result = []
+	while first_el < 1:
+			first_el += 1
+			second_el = 0
+			while second_el < 78:
+					second_el += 1
+					third_el = 0
+					fourth_el = 0
+					while third_el < 255:
+							third_el += 1
+							fourth_el = 1
+							while fourth_el < 255:
+									result.append(f'{first_el}.{second_el}.{third_el}.{fourth_el}')
+									fourth_el += 1
+					result.append(f'{first_el}.{second_el}.{third_el}.{fourth_el}')
+	return result
+
 
 
 ips = generate_ipv4_list()
 
 
 def write_ips(doc):
-		i = 1
-		file = open("ipv4_addresses.txt", "w+")
-		for j in doc[0:5000000]:
-				file.write(f'{i};{j}\n')
-				i += 1
-		file.close()
+	i = 1
+	file = open("ipv4_addresses.txt", "w+")
+	for j in doc[0:5000000]:
+		file.write(f'{i};{j}\n')
+		i += 1
+	file.close()
 
 
 write_ips(ips)
