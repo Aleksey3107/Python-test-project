b4bbf3c6f2d9a976c0fa2ee28ac9416962808d8b
alexey.sergeenko <alexey.sergeenko@idfinance.com>
2020-07-19 17:07:29 +0300

    added ipv6 generator

--- exercises/generate_ip.py
+++ exercises/generate_ip.py
@@ -1,17 +1,27 @@
 import random
+import string
 
 
 def generate_ipv4_list():
 	data = set()
 	while len(data) <= 5000000:
 		data.add('.'.join('%s' % random.randint(0, 256) for i in range(4)))
+		print(len(data))
 	return data
 
 
-ips = generate_ipv4_list()
+def generate_ipv6_list():
+	data = set()
+	while len(data) <= 5000000:
+		data.add("2001:cafe:" + ":".join(("%x" % random.randint(0, 16**4) for i in range(6))))
+	return data
 
 
-def write_ips(doc):
+# ipv4 = generate_ipv4_list()
+ipv6 = generate_ipv6_list()
+
+
+def write_ipv4(doc):
 	i = 1
 	file = open("ipv4_addresses.txt", "w+")
 	for j in doc:
@@ -20,4 +30,14 @@ def write_ips(doc):
 	file.close()
 
 
-write_ips(ips)
+def write_ipv6(doc):
+	i = 1
+	file = open("ipv6_addresses.txt", "w+")
+	for j in doc:
+		file.write(f'{i};{j}\n')
+		i += 1
+	file.close()
+
+
+# write_ipv4(ipv4)
+write_ipv6(ipv6)
