7266b879c0196fdd10bdd3a0028095318eb865d2
alexey.sergeenko <alexey.sergeenko@idfinance.com>
2020-09-08 18:40:08 +0300

    hackerrank exercises

--- HackerRank/StudentsSubscriptionIntersection.py
+++ HackerRank/StudentsSubscriptionIntersection.py
@@ -4,4 +4,4 @@ students_eng = set(map(int, (input().split())))
 count_students_fr = int(input())
 students_fr = set(map(int, (input().split())))
 
-print(len(students_eng.difference(students_fr)))
+print(len(students_eng.intersection(students_fr)))
