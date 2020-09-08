a47f4cc96061ee10aa98ca85c1480376d1370d70
alexey.sergeenko <alexey.sergeenko@idfinance.com>
2020-09-08 18:28:06 +0300

    update Subset.py

--- HackerRank/Subset.py
+++ HackerRank/Subset.py
@@ -1,3 +1,7 @@
-num_testcases = int(input())
-num_of_els_in_values_one = int(input())
-values_one = set(map(int, input().split()))
+# num_testcases = int(input())
+for i in range(int(input())):
+	num_elements_a = int(input())
+	elements_a = set(map(int, input().split()))
+	num_elements_b = int(input())
+	elements_b = set(map(int, input().split()))
+	print(True) if elements_a.issubset(elements_b) else print(False)
